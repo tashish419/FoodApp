07ae97cce86ab2436286f0d17dadb6a990580d5c
tashish419 <tashish410gmail.com>
2023-10-04 01:28:40 +0530

    Title change

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 const About = () => {
   return (
     <div className="about container">
-      <h1 className="about-heading">SindhBhoj</h1>
+      <h1 className="about-heading">ProFood</h1>
       <p className="about-para">
         This is a food App created by Ashish Tomar.It has some awesome features
         like search, filter where u can find easily your favourite restraunts which offer
